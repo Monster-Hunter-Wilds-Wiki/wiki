08b43e7dfe2a833e54d918ee553392b8ae5d5a65
PierreT <pierre.tusseau@gmail.com>
2024-12-12 14:32:16 +0100

    Example as default skin

--- config/skins.php
+++ config/skins.php
@@ -3,6 +3,7 @@
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
 $wgDefaultSkin = 'vector-2022';
+// $wgDefaultSkin = 'example';
 
 # Enabled skins.
 # The following skins were automatically enabled:
