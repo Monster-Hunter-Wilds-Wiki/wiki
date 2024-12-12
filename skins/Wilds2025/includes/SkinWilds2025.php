d9d9929d8530aea27ab90e7a8892c66f4165a388
PierreT <pierre.tusseau@gmail.com>
2024-12-12 14:35:43 +0100

    Relative skin path, SkinMustache extended

--- skins/Wilds2025/includes/SkinWilds2025.php
+++ skins/Wilds2025/includes/SkinWilds2025.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Skins\Wilds2025;
 
+use MediaWiki\MainConfigNames;
+use MediaWiki\Skin\SkinComponentUtils;
 use SkinMustache;
 
 /**
@@ -11,8 +13,22 @@ class SkinWilds2025 extends SkinMustache
 {
   public function getTemplateData()
   {
+    // http://localhost/mhwilds_wiki_sandbox/index.php/Main_page?useskin=skinjson
     $data = parent::getTemplateData();
+    # Example
     $data['html-myskin-hello-world'] = '<strong>Hello world!</strong>'; // or $this->msg('msg-key')->parse();
+    # Custom data set
+    # ------------------------------------
+    // Resources
+    $data['resources'] = $GLOBALS['wgResourceBasePath'] . '/resources';
+    // User
+    $data['user'] = [
+      "name" => $this->username,
+      "page_uri" => $this->userpageUrlDetails['href'],
+      // "talk_uri" => $this->getConfig()->get(MainConfigNames::ScriptPath),
+    ];
+    $data['user-menu'] = $data['data-portlets']['data-user-menu']['array-items'];
+
     return $data;
   }
 }
